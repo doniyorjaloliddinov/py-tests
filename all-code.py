73cf1d2098b50f8740fce4010c1fce751ac31859
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-04-11 01:10:22 +0500

    add two comparative codes

--- all-code.py
+++ all-code.py
@@ -5,3 +5,21 @@ Created on Mon Apr 11 00:39:53 2022
 @author: user
 """
 
+#age comparing 
+age = 16
+user_age = int(input("Yoshingizni kiriting: "))        
+if age <= user_age:
+    print("Sizga musobaqada qatnashish mumkin")
+else:
+    print("Hali yoshlik qilar ekansansiz")
+#//age comparing
+
+
+# cost of phone, is user money enough to buy it
+money = 100
+get_money = int(input("Qacha pullingiz bor: "))   
+if money <= get_money:
+    print("Telefon sizniki")
+else:
+    print("Afsus oylik chiqishini kutar ekansiz")    
+#//cost of phone, is user money enough to buy it    
